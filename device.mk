8b7af46405dba2e20a8531e572fa9eb577d6b1be
root <root@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-11 04:28:34 +0000

    test audio bluetooth

--- device.mk
+++ device.mk
@@ -165,7 +165,6 @@ PRODUCT_BOOT_JARS += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    BluetoothQti \
     libbthost_if \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
