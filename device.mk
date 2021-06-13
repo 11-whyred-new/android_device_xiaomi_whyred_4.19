pranaya2005/android_device_xiaomi_whyred

whyred: Enable power-libperfmgr
Change-Id: Ia5f8fb4a2afc7a453eb09d2092a7872fd8442bc9
Signed-off-by: Pranaya Deomani <deomanipranaya007@gmail.com>

device.mk
@@ -364,7 +364,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.3-service.whyred-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf

11-whyred-new/android_device_xiaomi_whyred_4.19

device.mk
@@ -400,7 +400,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf